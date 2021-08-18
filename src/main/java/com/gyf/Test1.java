7b785bab6d11179247d0821cb475c6cbb08d7bd3
XiaoYu19910407 <jzybgmxy123456>
2021-08-18 19:06:17 +0800

    测试1

---
+++ src/main/java/com/gyf/Test1.java
@@ -0,0 +1,11 @@
+package com.gyf;
+
+/**
+ * @author xiaoyu
+ * @date 2021/08/18 19:05:57
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
